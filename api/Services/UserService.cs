bc893b662e6a8fad0d42d050b777fa073efa638f
Jerie See Villanueva <codestackjs007@gmail.com>
2024-07-30 20:08:14 -0700

    added database context, started controller with our add user and service

--- api/Services/UserService.cs
+++ api/Services/UserService.cs
@@ -2,10 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Models.DTO;
+using api.Services.Context;
 
 namespace api.Services;
 
 public class UserService
 {
+    private readonly DataContext _context;
 
+    public UserService(DataContext context)
+    {
+        _context = context;
+    }
+
+    //helper functions to help us check if the user exist 
+    //DoesUserExist 
+    public bool DoesUserExist(string username)
+    {
+        //check out tables to see if the user name exist
+        //if one item matches our condition that item will be returned
+        // if no items matches it will return null
+        //if multiple items match it will return an error
+    }
+
+    public bool AddUser(CreateAccountDTO userToAdd)
+    {
+       //if the user already exist
+       //if the do not exist we add an account
+       //Else throw a false
+    }
 }
